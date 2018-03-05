226c0792923d815c35f0c0e0ea863d40e77de90b
zhangxiang <958033967@qq.com>
2018-03-05 23:25:47 +0800

    process

--- Socket1_WebServer/source/muti_process/master.js
+++ Socket1_WebServer/source/muti_process/master.js
@@ -2,7 +2,23 @@ const cp = require('child_process');
 const fork = cp.fork;
 const os = require('os');
 const cpus = os.cpus();
+const Net = require('net');
+
+const muti_process = [];
+const port = 1337;
+const host = '127.0.0.1';
+const server = new Net.Server();
 
 for(let i = 0, length = cpus.length; i < length; i ++) {
-  fork('../server.js');
+  muti_process.push(fork('./server.js'));
 }
+
+server.listen(port, host, () => {
+  // muti_process.forEach((process) => {
+  //   process.send('server', server);
+  // });
+  muti_process[0].send('server', server);
+  muti_process[1].send('server', server);
+  server.close();
+  console.log(`server listen on port ${port}`);
+});
---
+++ Socket1_WebServer/source/server.js
@@ -0,0 +1,49 @@
+const Net = require('net');
+
+const port = process.port || 8080;
+const host = '127.0.0.1';
+
+const server = new Net.Server();
+
+function sleep (second) {
+  second = parseInt(second, 10);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, second * 1000);
+  });
+}
+
+let allSpend = 0;
+
+server.on('connection', (socket) => {
+  console.log('a new connection has been established.');
+  const start = Date.now();
+  console.log('start time:', start);
+  socket.on('data', (chunk) => {
+    console.log(`received data from client...`);
+    response(chunk);
+  });
+
+  socket.on('end', () => {
+    console.log('Closing connection with the client.\n');
+  });
+
+  async function response (chunk) {
+    console.log(`wait for ${ allSpend } s....`);
+    await sleep(3);
+    socket.write(chunk.toString());
+    socket.end();
+    console.log(`request spend ${Math.floor( ( Date.now() - start ) / 1000 ) } s`);
+    allSpend += 3;
+  }
+
+});
+
+server.on('error', (err) => {
+  console.log(err);
+});
+
+server.listen(port, host, () => {
+  console.log(`server is runing on port ${port}\n`);
+});

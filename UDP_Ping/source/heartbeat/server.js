e93d59d3bcd583d69fd0eb865ddb8799203895d6
zhangxiang <958033967@qq.com>
2018-03-21 23:39:35 +0800

    heartbeat

--- UDP_Ping/source/heartbeat/server.js
+++ UDP_Ping/source/heartbeat/server.js
@@ -1,21 +1,27 @@
 const dgram = require('dgram');
 const socket = dgram.createSocket('udp4');
 
+const heartbeat = {};
+const checkPeriod = 150;
+const checkTimeout = 60000;
+
 socket.on('message', (msg, receiver) => {
   let rand = Math.round( Math.random() * 10 );
   if(rand < 4) {
     console.log('请求超时');
     return;
   }
-  console.log(`receiver msg: ${msg} from ${receiver.address}:${receiver.port}`);
+  const { address, port } = receiver;
+  console.log(`receiver msg: ${msg} from ${address}:${port}`);
   msg = msg.toString().split(' ');
   let sendMessage = `received Ping ${msg[1]}`;
-  socket.send(sendMessage, 0, sendMessage.length, receiver.port, receiver.address, (err, bytes) => {});
+  heartbeat[`${address}:${port}`] = new Date();
+  // socket.send(sendMessage, 0, sendMessage.length, receiver.port, receiver.address, (err, bytes) => {});
 });
 
 socket.on('listening', () => {
-  const address = socket.address();
-  console.log(`server is listening on ${address.address}:${address.port}`);
+  const { address, port } = socket.address();
+  console.log(`server is listening on ${address}:${port}`);
 });
 
 socket.on('close', () => {
@@ -27,3 +33,12 @@ socket.on('error', (err) => {
 });
 
 socket.bind(8900);
+
+setInterval(() => {
+  Object.keys(heartbeat).forEach((address) => {
+    let time = heartbeat[address];
+    if(new Date() - time < checkTimeout) {
+      console.log(`${address} slient.`);
+    }
+  });
+}, checkPeriod);
